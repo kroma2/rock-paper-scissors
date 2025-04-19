e82cdde92d52b1981c9b739e1a61d0ff22758db4
kroma2 <janickijakub05@gmail.com>
2025-04-19 16:20:26 +0200

    added function for human item choice

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function GetComputerChoice() {
     randomNumber = Math.floor(Math.random() * 3);
  
@@ -19,3 +21,22 @@ function GetComputerChoice() {
 
 console.log(GetComputerChoice())
 
+
+
+function GetHumanChoice() {
+    let item = prompt('Rock, paper or scissors?')
+    if (item.toLowerCase === 'rock'.toLowerCase){
+        return 'You chose rock'
+    }
+    else if (item.toLowerCase === 'paper'.toLowerCase){
+        return 'You chose paper'
+    }
+    else if (item.toLowerCase === 'scissors'.toLowerCase){
+        return 'You chose scissors'
+    }
+    else{
+        return 'Please choose rock, paper or scissors'
+    }
+}
+
+console.log(GetHumanChoice())
