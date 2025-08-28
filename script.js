695843a1fbf731422ee636c56a96451088e0076b
kroma2 <janickijakub05@gmail.com>
2025-08-28 10:50:47 +0200

    declaring winner logic

--- script.js
+++ script.js
@@ -17,16 +17,28 @@ function GetComputerChoice() {
     }
 }
 
+const humanChoiceSpan = document.querySelector("#player-choice");
+const pcChoiceSpan = document.querySelector("#pc-choice");
+const playerScoreSpan = document.querySelector("#player-score")
+const pcScoreSpan = document.querySelector ("#pc-score")
+const announce = document.querySelector("#announce-winner");
+const playedRound = document.querySelector('#played-round')
+
+function resetGame() {
+    announce.textContent = 'PICK ONE BELOW'
+    announce.style.color = 'black'
+    humanScore = 0;
+    computerScore = 0;
+    humanChoiceSpan.textContent = '—'
+    pcChoiceSpan.textContent = '—'
+    playerScoreSpan.textContent = '0'
+    pcScoreSpan.textContent = '0'
+}
+
     let humanScore = 0;
     let computerScore = 0; 
 
     function playRound(humanChoice, computerChoice) {
-        const humanChoiceSpan = document.querySelector("#player-choice");
-        const pcChoiceSpan = document.querySelector("#pc-choice");
-        const playerScoreSpan = document.querySelector("#player-score")
-        const pcScoreSpan = document.querySelector ("#pc-score")
-        const announce = document.querySelector("#announce-winner");
-        const playedRound = document.querySelector('#played-round')
 
         computerChoice = GetComputerChoice();
         humanChoiceSpan.textContent = humanChoice;
@@ -54,7 +66,11 @@ function GetComputerChoice() {
         } 
 
         playedRound.style.transform = 'scale(1.2)'; setTimeout(() => playedRound.style.transform = 'scale(1)', 150);
-
+        if(humanScore === 5 || computerScore === 5)
+        {
+            humanScore === 5 ? alert('You won!') : alert('You lost')
+            resetGame();
+        }
     }
 
 
