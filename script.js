99532760ec2a3c74e5c1d6562e913e11330d4406
kroma2 <janickijakub05@gmail.com>
2025-08-28 09:52:09 +0200

    dynamically change score

--- script.js
+++ script.js
@@ -21,15 +21,17 @@ function GetComputerChoice() {
     let computerScore = 0; 
 
     function playRound(humanChoice, computerChoice) {
-        
         const humanChoiceSpan = document.querySelector("#player-choice");
         const pcChoiceSpan = document.querySelector("#pc-choice");
+        const playerScoreSpan = document.querySelector("#player-score")
+        const pcScoreSpan = document.querySelector ("#pc-score")
+        const announce = document.querySelector("#announce-winner");
 
         computerChoice = GetComputerChoice();
         humanChoiceSpan.textContent = humanChoice;
         pcChoiceSpan.textContent = computerChoice;
 
-        const announce = document.querySelector("#announce-winner");
+        
 
         if(humanChoice === computerChoice){
             announce.textContent = 'Round tied';
@@ -41,15 +43,15 @@ function GetComputerChoice() {
             announce.textContent = 'Round won';
             announce.style.color = 'green'
             humanScore++;
+            playerScoreSpan.textContent = humanScore;
         }
         else{
             announce.textContent = 'Round lost';
             announce.style.color = 'red'
             computerScore++;
+            pcScoreSpan.textContent = computerScore;
         }
 
-        console.log(humanScore);
-        console.log(computerScore);
     }
 
 
